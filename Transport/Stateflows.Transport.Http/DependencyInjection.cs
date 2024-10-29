9060703d19b0db408448ad368845aa7505b157ba
radek00 <76593350+radek00@users.noreply.github.com>
2024-10-29 08:51:51 +0100

    Perform policy based authorization on transport http controller (#31)
    * add registration for auth interceptor
    
    * perfrom authorization on controler level
    
    * add comment

--- Transport/Stateflows.Transport.Http/DependencyInjection.cs
+++ Transport/Stateflows.Transport.Http/DependencyInjection.cs
@@ -8,6 +8,7 @@ using Stateflows.Common.Utilities;
 using Stateflows.Common.Interfaces;
 using Stateflows.Common.Extensions;
 using Stateflows.Common.Transport.Classes;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Stateflows.Transport.Http
 {
@@ -28,11 +29,14 @@ namespace Stateflows.Transport.Http
                         INotificationsHub hub
                     ) =>
                     {
+
                         var responseTime = DateTime.Now;
                         using var reader = new StreamReader(context.Request.Body);
                         var body = await reader.ReadToEndAsync();
                         var input = StateflowsJsonConverter.DeserializeObject<StateflowsRequest>(body);
-
+                        //temporary authorization solution
+                        if (!AuthorizeUser(context, input.Event))
+                            return Results.Unauthorized();
                         var behaviorId = new BehaviorId(input.BehaviorId.Type, input.BehaviorId.Name, input.BehaviorId.Instance);
                         if (locator.TryLocateBehavior(behaviorId, out var behavior))
                         {
@@ -76,6 +80,26 @@ namespace Stateflows.Transport.Http
             }
 
             return builder;
+        }
+
+        private static bool AuthorizeUser(HttpContext context, Event stateflowsEvent)
+        {
+            AuthorizeAttribute? authAttribute = (AuthorizeAttribute?)Attribute.GetCustomAttribute(stateflowsEvent.GetType(),
+                                                                                                  typeof(AuthorizeAttribute));
+            if (authAttribute != null)
+            {
+                var policy = authAttribute.Policy;
+                var user = context.User;
+                if (policy == null && user != null && user.Identity!.IsAuthenticated)
+                    return true;
+                if (user != null && user.Claims.Any(c => c.Value.Equals(policy)))
+                {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+
         }
     }
 }
